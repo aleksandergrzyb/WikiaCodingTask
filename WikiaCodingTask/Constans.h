4a019b791efcff5b997ca0ca4bf9224a4a0a4f36
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2015-07-15 11:58:36 +0200

    initial commit

--- WikiaCodingTask/Constans.h
+++ WikiaCodingTask/Constans.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  Constans.h
 //  WikiaCodingTask
 //
 //  Created by Aleksander Grzyb on 15/07/15.
 //  Copyright (c) 2015 Aleksander Grzyb. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#ifndef WikiaCodingTask_Constans_h
+#define WikiaCodingTask_Constans_h
 
-@interface ViewController : UIViewController
 
-
-@end
-
+#endif
