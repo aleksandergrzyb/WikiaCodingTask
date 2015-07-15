a5b7ea4fb9740774c6932e68873b3e81eeda6465
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2015-07-15 11:43:21 +0200

    Initial Commit

---
+++ WikiaCodingTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WikiaCodingTask
+//
+//  Created by Aleksander Grzyb on 15/07/15.
+//  Copyright (c) 2015 Aleksander Grzyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WikiaCodingTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WikiaCodingTask
+//
+//  Created by Aleksander Grzyb on 15/07/15.
+//  Copyright (c) 2015 Aleksander Grzyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
